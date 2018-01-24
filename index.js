5739b115de1ae5b1c16ba4141e8f3517b8e61c10
Justin Daniel <justinddaniel@gmail.com>
2018-01-24 23:22:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function init() {
   Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
   function renderMain() {
   var template = Handlebars.compile(document.getElementById("recipe-details-partial").innerHTML);
-  var html = template({name: 'Gordon Ramsay'});
+  var html = template({name});
 }
 }
 document.addEventListener("DOMContentLoaded", function(event) {
