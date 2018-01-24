52e0b48b358bb8a421a2722edd3263fd672cd7ce
Justin Daniel <justinddaniel@gmail.com>
2018-01-24 23:22:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 function init() {
-  //put any page initialization/handlebars initialization here
+  Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
+  function renderMain() {
+  var template = Handlebars.compile(document.getElementById("recipe-details-partial").innerHTML);
+  var html = template({name: 'Gordon Ramsay'});
+}
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
