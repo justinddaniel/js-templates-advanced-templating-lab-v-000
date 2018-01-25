96e1fe5bf1f093e62851a44b3c4c6b2416f05903
Justin Daniel <justinddaniel@gmail.com>
2018-01-25 02:42:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,20 +1,5 @@
 function init() {
-  Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
-  function renderMain() {
-  var recipe = {
-    name: 'chicken noodle soup'
-    description: 'yummy chicken noodle soup',
-    ingredients: [
-      {quantity: "1 cup", name: 'chicken'},
-      {quantity: "3 nanoliters", name: 'stock'},
-      {quantity: "12", name: 'noodles'},
-      {quantity: "5 grams", name: 'salt'},
-      {quantity: "1 microgram", name: 'pepper'}
-    ]
-  }
-  var template = Handlebars.compile(document.getElementById("recipe-details-partial").innerHTML);
-  var html = template({recipe});
-}
+
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
