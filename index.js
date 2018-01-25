8c9deb704734617ea3d04ae09fdb111efa4a142c
Justin Daniel <justinddaniel@gmail.com>
2018-01-25 01:25:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function init() {
   Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
   function renderMain() {
   var template = Handlebars.compile(document.getElementById("recipe-details-partial").innerHTML);
-  var html = template({name});
+  var html = template({context});
 }
 }
 document.addEventListener("DOMContentLoaded", function(event) {
